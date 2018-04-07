86f970dbfadda84a0c2b687c5a540ba7da767d9f
jjreich <jj.reich@nalusolutions.com>
2018-04-06 22:44:41 -0500

    re-added gameRating calculations

--- app/models/player_game_subdatum.rb
+++ app/models/player_game_subdatum.rb
@@ -8,6 +8,30 @@ class PlayerGameSubdatum < ApplicationRecord
     end
 
     def gameRating
+    	passingRating = passingPercentage * completedPasses * 0.002
+    	significantPassingRating = (corners + crosses + clearings + longBalls + thruBalls) * 0.5
+    	keyPassRating = keyPasses * 1
+    	turnoverRating = turnovers * -0.3
+    	dribbleRating = dribbles * 0.3
+    	dispossessedRating = dispossessed * -0.3
+    	fouledRating = fouled * 1
+    	shotsRating = (shots - shotsOnTarget) * -0.1
+    	shotsOnTargetRating = shotsOnTarget * 0.2
+    	assistRating = assists * 3
+    	goalRating = goals * 5
+    	fiftyFiftyRating = fiftyFiftiesWon * 0.3
+    	tackleRating = tackles * 0.6
+    	dribblePastRating = dribblePast * -0.75
+    	interceptionRating = interceptions * 0.4
+    	recoveriesRating = recoveries * 0.2
+    	blockedShotRating = blockedShots * 0.5
+    	ownGoalRating = ownGoals * -5.0
+   		savesRating = saves * 2
+   		foulsRating = (fouls * -1) + (yellowCards * -5) + (redCards * -10)
+   		offsideRating = offsides * -0.5
 
+   		offensiveRating = passingRating + significantPassingRating + keyPassRating + turnoverRating + dribbleRating + dispossessedRating + fouledRating + shotsRating + shotsOnTargetRating + goalRating + assistRating + offsideRating
+    	defensiveRating = fiftyFiftyRating + tackleRating + dribblePastRating + interceptionRating + recoveriesRating + blockedShotRating + ownGoalRating + foulsRating + savesRating
+    	gameRating = offensiveRating + defensiveRating
     end
 end
