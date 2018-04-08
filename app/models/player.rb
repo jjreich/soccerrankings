f96ceb9fd20048a1ada4016a50cbab320132e466
jjreich <jj.reich@nalusolutions.com>
2018-04-07 22:44:39 -0500

    first effort to generate average rating is created.

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,9 +3,20 @@ class Player < ApplicationRecord
 	has_many :player_game_subdata
 
 	def fiveGameAverage
-		played_game_ratings = PlayerGameSubdatum.where(:player_id => id).pluck(:gameRating)
+		played_game_ratings = PlayerGameSubdatum.where(:player_id => id)
+
+		player_game_count = PlayerGameSubdatum.where(:player_id => id).count
+
+		playerStatsTotal = 0
+		played_game_ratings.each do |playerStats|
+			playerStatsTotal += playerStats.gameRating
+		end
 	
-		fiveGameAverage = played_game_ratings.gameRating
+		if (player_game_count == 0)
+			fiveGameAverage = 0
+		else
+			fiveGameAverage = playerStatsTotal / player_game_count
+		end
 	end
 
 	def tenGameAverage
